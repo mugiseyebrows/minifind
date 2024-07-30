b6b226295d25e526a0d5bfd9070a029e94584d33
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-30 13:00:26 +0300

    .mtime() and .first()

--- minifind/__init__.py
+++ minifind/__init__.py
@@ -3,6 +3,7 @@ from collections.abc import Callable
 from typing import Union
 import fnmatch
 import re
+import datetime
 
 Predicate = Callable[[str, str], bool]
 
@@ -77,6 +78,22 @@ class ExtPredicate:
                 raise ValueError("expected str or re.Pattern as pattern, got {}".format(type(pattern)))
         return False
 
+class MtimePredicate:
+    def __init__(self, low, high):
+        if low is None and high is None:
+            raise ValueError("both low and high is None, predicate is useless")
+        if isinstance(low, datetime.datetime):
+            low = low.timestamp()
+        if isinstance(high, datetime.datetime):
+            high = high.timestamp()
+        self._low = low
+        self._high = high
+    def __call__(self, name: str, path: str):
+        mtime = os.path.getmtime(path)
+        low = self._low
+        high = self._high
+        return (low is None or mtime > low) and (high is None or mtime < high)
+
 def matches(name, path, predicates: list[Predicate]):
     for predicate in predicates:
         if not predicate(name, path):
@@ -99,6 +116,7 @@ class Find:
         self._do_files = False
         self._do_dirs = False
         self._maxdepth = 0
+        self._first = 0
 
     def files(self):
         self._do_files = True
@@ -119,13 +137,21 @@ class Find:
     def size(self, low = None, high = None):
         self._predicates.append(SizePredicate(low, high))
         return self
+    
+    def mtime(self, low = None, high = None):
+        self._predicates.append(MtimePredicate(low, high))
+        return self
 
     def filter(self, predicate: Predicate):
         self._predicates.append(predicate)
         return self
     
-    def maxdepth(self, n : int):
+    def maxdepth(self, n: int):
         self._maxdepth = n
+        return self
+
+    def first(self, n: int):
+        self._first = n
         return self
 
     def for_each(self, callback: Callback):
@@ -134,6 +160,7 @@ class Find:
         if not do_dirs and not do_files:
             do_dirs = True
             do_files = True
+        count = 0
         for path in self._paths:
             for root, dirs, files in os.walk(path):
                 if self._maxdepth > 0 and path_depth(root, path) == self._maxdepth:
@@ -145,11 +172,17 @@ class Find:
                         p = os.path.join(root, f)
                         if matches(f, p, self._predicates):
                             callback(f, p)
+                            count += 1
+                            if count == self._first:
+                                return
                 if do_dirs:
                     for f in dirs:
                         p = os.path.join(root, f)
                         if matches(f, p, self._predicates):
                             callback(f, p)
+                            count += 1
+                            if count == self._first:
+                                return
 
     def collect(self) -> list[str]:
         collector = Collector()
